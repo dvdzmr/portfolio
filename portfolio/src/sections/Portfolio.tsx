4cf632b40b81928b7de39e460d19c269de066b64
David <51931592+dvdzmr@users.noreply.github.com>
2024-04-21 17:38:16 +0300

    new

--- portfolio/src/sections/Portfolio.tsx
+++ portfolio/src/sections/Portfolio.tsx
@@ -1,16 +1,47 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import { useState } from 'react';
+
 
 function ResponsiveAutoExample() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <Container>
-      <Row>
-        <Col sm><img src="../media/portfolio/cabin.png"></img></Col>
-        <Col sm>sm=true</Col>
-        <Col sm>sm=true</Col>
+    <>
+
+    <Container>   
+    <Row className="Portfolio_row">
+
+    {/* for loop */}
+
+    <Col sm> 
+      <a className="d-block mb-4 h-100" onClick={handleShow}>
+          <img className="img-fluid img-thumbnail" src="https://source.unsplash.com/pWkk7iiCoDM/400x300" alt=""/>
+        </a>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      </Col>
+
+
       </Row>
-    </Container>
+      </Container>
+
+    </>
   );
 }
 
