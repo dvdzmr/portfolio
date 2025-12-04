49ecb82467753ddeea6d48c75eda79929aaef652
David <51931592+dvdzmr@users.noreply.github.com>
2025-12-04 14:42:21 +0200

    Updates

--- _next/static/chunks/pages/_app-cf1b06b21553ade2.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{1439:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(6367)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(1439),n(1509))),_N_E=_.O()}]);
--- _next/static/chunks/pages/_error-a0640d60775a8789.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[731],{2177:(_,n,e)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return e(2870)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[636,593,792],()=>n(2177)),_N_E=_.O()}]);
